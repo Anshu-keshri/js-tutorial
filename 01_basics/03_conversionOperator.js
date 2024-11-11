74e10de77535f74ba914d0a8aa969c7026b07a4b
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-11 17:47:55 +0000

    conversion of data type in js

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,16 @@
+let age="33";
+let city="patna"
+
+console.log(typeof(age))
+console.log(typeof(age))
+
+
+let convertToNumber=Number(age)
+
+console.log(convertToNumber)
+
+let convertToNum=Number(city)
+
+console.log(typeof(convertToNumber))
+console.log(typeof(convertToNum))
+console.log(convertToNum)  //output will be NaN  mean  not a number.

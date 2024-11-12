37636f5e0e5621a39e91d672be6080fa0697ee3c
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-12 07:16:38 +0000

    conversion of data types in js

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -14,3 +14,14 @@ let convertToNum=Number(city)
 console.log(typeof(convertToNumber))
 console.log(typeof(convertToNum))
 console.log(convertToNum)  //output will be NaN  mean  not a number.
+
+//some confusing things when adding  strigs and number.
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2+3);
+console.log(1+2+"2");
+console.log(1+2+"2"+2);
+
+console.log("abc"+2);
+console.log(1+2+"abc");

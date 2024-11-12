192fb576d27e46fcd3e2f1fe4bdcfb2b56a62833
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-12 07:17:08 +0000

    comparison in js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+/*
+console.log("2">1);
+console.log("2"<1);
+console.log("2"==1); 
+*/
+
+
+//...............REMEMBER IN JS COMPARISON AND EQUALITY BOTH ARE DIFFERENT THINGS............
+console.log(null>0);  //converts null into 0
+console.log(null<0);   //converts null into 0
+console.log(null==0);
+console.log(null>=0);   //converts null into 0
+console.log(null<=0);   //converts null into 0
+
+//strict check (===).....it also checks data type during comparison.
+console.log("2"===2);
+console.log("2"==2); //see the difference between (==)  and  (===).
+console.log(2===2);
+

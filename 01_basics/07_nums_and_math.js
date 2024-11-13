695a33d1d3c30d04c98576f3819f1bb47d51ec67
Anshu-keshri <anshukeshri543@gmail.com>
2024-11-13 11:42:03 +0000

    some imp. properties of math and numbers.

---
+++ 01_basics/07_nums_and_math.js
@@ -0,0 +1,18 @@
+const score=100;
+console.log(score);
+
+const balance=new Number(400);
+console.log(balance);
+
+console.log(balance.toString());  //converts to string.
+console.log(typeof(balance.toString()));
+
+console.log(balance.length);
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));  // used to get more precise value...mainly used in calculations and ecom websites.
+
+const hundreds=1000000;
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
+
